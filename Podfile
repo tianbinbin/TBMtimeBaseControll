2c6f680461057eeeb0ff6e3f21ab15e79d9c7fb7
tianbinbin <15711163094@163.com>
2020-12-22 11:04:28 +0800

    Feature: 初始化项目

---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+# 此处是管理cocoapod第三方库的显示状态 加上这两句之后 看起来更有层次了
+install! 'cocoapods', :generate_multiple_pod_projects => true
+
+platform :ios, '11.0'
+use_frameworks!
+
+target 'TBMtimeBaseControll' do
+
+  pod 'Alamofire', '5.2.1'	
+
+end
---
+++ TBMtimeBaseControll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TBMtimeBaseControll
+//
+//  Created by 田彬彬 on 2020/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
