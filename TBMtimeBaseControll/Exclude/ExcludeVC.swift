9d0d0578a030eb975865dff71c39fcd4d348f0da
tianbinbin <15711163094@163.com>
2020-12-22 14:30:06 +0800

    Feature: 引入网络基础库

---
+++ TBMtimeBaseControll/Exclude/ExcludeVC.swift
@@ -0,0 +1,20 @@
+//
+//  ExcludeVC.swift
+//  MtimeBaseControll
+//
+//  Created by 田彬彬 on 2020/12/10.
+//
+
+import UIKit
+import Foundation
+
+class ExcludeVC:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        print("被排除在外的文件，出了这个工程不会被任何人饮用")
+        
+    }
+}
